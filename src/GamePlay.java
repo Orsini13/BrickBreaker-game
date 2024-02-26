ef0d673ca2a20ba1f9a50d2925ca935ca4cd2c2f
Udeh Udochukwu <udochukwugodwin13@gmail.com>
2024-02-26 23:47:29 +0100

    First commit

---
+++ src/GamePlay.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+//keylistener and actionlistener are used to determine the movement and direction of the ball
+//they both had to be imported and implemented
+public class GamePlay extends JPanel implements KeyListener, ActionListener {
+
+    private boolean play = false;
+    private int score = 0;
+    private int totalBricks = 21;
+    private Timer timer;
+    private int delay = 8;
+
+    private int playerX = 310;
+    private int ballposX = 120;
+    private int ballposY = 350;
+    private int ballXdir = -1;
+    private int ballYdir = -2;
+
+    public GamePlay(){
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer = new Timer(delay, this);
+        timer.start();
+    }
+    public void print(Graphics g){
+        g.setColor((Color.white));
+        g.fillRect(1,1,692,592);
+        g.setColor(Color.yellow);
+        g.fillRect(0,0,3,562);
+        g.fillRect(0,0,692,3);
+        g.fillRect(691,0,3,592);
+
+        g.setColor(Color.blue);
+        g.fillRect(playerX,550,100,8);
+
+        g.setColor(Color.green);
+        g.fillOval(ballposX, ballposY,20,20);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
